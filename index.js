f87d98de1f6fdad0b0be0e40002ec3623f8b9e44
gemaquejr <gemaquejr@hotmail.com>
2022-05-04 23:22:26 -0300

    Adiciona requisito 1

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('./talker.json', 'utf8');
+  if (data.length === 0) {
+    return res.status(200).json(JSON.parse(data));
+  }
+  res.status(200).json(JSON.parse(data));
+  });
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
