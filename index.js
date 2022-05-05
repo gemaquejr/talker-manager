a36c5e4b371317e6cd53862d92c5df7092267267
gemaquejr <gemaquejr@hotmail.com>
2022-05-05 19:36:07 -0300

    Adiciona requisito 2

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const fs = require('fs').promises;
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/talker', async (_req, res) => {
+// Ajuda do Pedro de Carvalho na Mentoria
+async function readTalkers() {
   const data = await fs.readFile('./talker.json', 'utf8');
+  return JSON.parse(data);
+}
+
+app.get('/talker', async (_req, res) => {
+  const data = await readTalkers();
   if (data.length === 0) {
-    return res.status(200).json(JSON.parse(data));
+    return res.status(200).json(data);
   }
-  res.status(200).json(JSON.parse(data));
+  res.status(200).json(data);
   });
+
+  app.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await readTalkers();
+    const datas = data.find((talker) => (talker.id === Number(id)));
+    if (!datas) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(datas);
+    });
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
